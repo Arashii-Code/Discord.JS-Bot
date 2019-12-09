7e324a3c50334d60d72e1904a4598f784c75ee4e
Arashii <44939502+Arashii-Code@users.noreply.github.com>
2019-12-09 19:57:52 +1300

    Update index.js

--- index.js
+++ index.js
@@ -1,55 +1,23 @@
-const Discord = require('discord.js');
-const fs = require("fs");
+const discord = require("discord.js");
 const config = require("./config.json");
-const bot = new Discord.Client({disableEveryone: true})
-const prefix = ">";
+const bot = new discord.Client({disableEveryone: true});
 
-// Command Hander
-bot.commands = new Discord.Collection();
-bot.aliases = new Discord.Collection();
-
-fs.readdir("./commands/", (err, files) => {
-
-    if(err) console.log(err)
-
-    let jsfile = files.filter(f => f.split(".").pop() === "js")
-    if(jsfile.length <= 0) {
-        return console.log("[LOGS] Couldn't Find Commands!");
-    }
-
-    jsfile.forEach((f, i) => {
-        let pull = require(`./commands/${f}`);
-        bot.commands.set(pull.config.name, pull);
-        pull.config.aliases.forEach(alias => {
-            bot.aliases.set(alias, pull.config.name)
-        });
-    })
-})
-
-//Ready Event
-bot.on('ready', () => {
-    bot.user.setStatus("dnd")
-bot.user.setActivity("Being Developed",          {type: "WATCHING"})
-
-console.log(`${bot.user.username} Is now online!`)
-
+bot.on("ready", async () => {
+  console.log(`${bot.user.username} is ready for action!`);
+  bot.user.setActivity("Minekwaft")
 });
 
-// Message Event
 bot.on("message", async message => {
+  if (message.author.bot) return;
+  if (message.channel.type === "dm") return;
 
-    if(message.author.bot) return;
-        if(message.channel.type === "dm") return;
-        if(!message.content.startsWith(prefix)) return;
+  let prefix = config.prefix;
+  let messageArray = message.content.split(" ");
+  let cmd = messageArray[0];
+  let args = messageArray.slice(1);
 
-    let messageArray = message.content.split(" ")
-    let cmd = messageArray[0].toLowerCase();
-    let args = messageArray.slice(1);
-    
-    
-    let commandfile = bot.commands.get(cmd.slice(prefix.length)) || bot.commands.get(bot.aliases.get(cmd.slice(prefix.length)))
-    if(commandfile) commandfile.run(bot,message,args)
-    
-})
+  if (cmd === `${prefix}tutorial`) {
+      return message.channel.send("Hello World");
+});
 
 bot.login(config.token);
